dd6eb11001cb6b3934c6f2df4ff42e12ef56eac8
sudhirtheindian <jaygovindpraj0603@gmail.com>
2024-06-17 11:37:07 +0530

    single child scrollview of widget

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Flutter App"),
+          title: Text("Flutter Apps"),
         ),
         body: SingleChildScrollView(
           child: Column(
